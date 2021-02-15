8e9c5b27d4c821b18f5b910ff5440f79e838dc38
carlos <carlosjmnz79@gmail.com>
2021-02-15 16:44:52 -0300

    settings.py cleaned

--- ret/settings.py
+++ ret/settings.py
@@ -8,13 +8,11 @@ dict_ = read_yaml(file_path)
 
 ENV = dict_['APP']['ENVIRONMENT']
 
-# section = 'mysql_' + ENV
 host = dict_['DATABASE']['HOST']
 user = dict_['DATABASE']['USERNAME']
 password = dict_['DATABASE']['PASSWORD']
 database = dict_['DATABASE']['DB']
 port = dict_['DATABASE']['PORT']
 ECHO = dict_['DATABASE']['ECHO']
-# ECHO = config_dict[section]['echo'] == 'True'
 DB_STR_CONNECTION = ("mysql+mysqlconnector:"
             f"//{user}:{password}@{host}:{port}/{database}")
