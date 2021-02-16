30530616bd83097f291275e36101be25c97df3e6
carlos <carlosjmnz79@gmail.com>
2021-02-16 18:50:30 -0300

    scheduler() max datetimeid

--- ret/scheduler.py
+++ ret/scheduler.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from sqlalchemy import (
+        func,
+        and_,
+    )
 from loguru import logger
 
 from giver_of_times import giver_of_times
+
+from tables import (
+        Overshooter,
+        Terrain,
+        Ret,
+        get_engine,
+        get_session,
+    )
 
 def scheduler(time_=None):
     if not time_:
@@ -19,6 +31,56 @@ def scheduler(time_=None):
 
     # de los anteriores seleccionar a los q min < tilt < max
     # (table rets) <-- tilt más reciente
+
+    engine = get_engine()
+    session = get_session(engine=engine)
+
+    # ---------------------------------------------------
+    max_overshooter = session.query(func.max(Overshooter.datetimeid)).first()
+    if not max_overshooter:
+        logger.info('No overshooters')
+        session.commit()
+        session.close()
+        return
+
+    logger.info('--->')
+
+
+   #  datetimeid_latest_overshooter
+
+   #          row = self.session.query(func.max(Tweet.status_id)).first()
+   #      if row is not None:
+   #          since_id = row[0] if row[0] is not None else 0
+   #      else:
+   #          since_id = 0
+
+
+   #  q = session.query(Overshooter,Terrain,Ret).filter(
+   #          Overshooter.)
+
+
+
+
+
+
+
+   #  exists = session.query(Transaction).filter(and_(
+   #                          Transaction.node==antenna.node,
+   #                          Transaction.cellname==antenna.cellname,
+   #                          Transaction.deviceno==antenna.deviceno,
+   #                              )
+   #                          ).first()
+
+   # from sqlalchemy_sample import session, User, Car
+
+   #  #On a single column
+   #  q = session.query(User, Car).filter(User.username==Car.owner_name)
+   #  for row in q:
+   #      print row
+    # ---------------------------------------------------
+
+    session.commit()
+    session.close()
 
     # entregar los candidatos a mid_term_evaluator()
 
